491f99513af26f91fd90293f958ce1e3c6510840
David DeGraw <david@custombit.com>
2014-12-08 22:53:54 -0700

    Create customers, referrers, and orders from order email.

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -4,6 +4,21 @@ class EmailProcessor
   end
 
   def process
-    Order.create(body: @email.body, email: @email.from[:email])
+    puts @email.subject
+    if @email.subject.include? 'Etsy Order Confirmation'
+      emails = @email.body.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i)
+      customer_email = emails.detect{ |e| emails.count(e) > 1}
+      referrer_email = emails.detect{ |e| emails.count(e) == 1}
+      referrer = nil
+      if referrer_email.present?
+        referrer = Customer.where(email: referrer_email).first
+        referrer = Customer.create(email: referrer_email) if referrer.nil?
+      end
+      customer = Customer.where(email: customer_email).first
+      customer = Customer.new(email: customer_email) if customer.nil?
+      customer.referred_by(referrer.id) if referrer.present?
+      customer.save
+      Order.create(customer: customer)
+    end
   end
 end
