7533b48047be5a519c8ce9ff006b9b28f1a8b62b
David DeGraw <david@custombit.com>
2014-12-09 21:34:11 -0700

    Temp remove broken code.

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -4,19 +4,21 @@ class EmailProcessor
   end
 
   def process
-    puts @email.subject
     if @email.subject.include? 'Etsy Order Confirmation'
+      # referrer_email = nil
       emails = @email.body.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i)
-      customer_email = emails.detect{ |e| emails.count(e) > 1}
-      referrer_email = emails.detect{ |e| emails.count(e) == 1}
-      referrer = nil
-      if referrer_email.present?
-        referrer = Customer.where(email: referrer_email).first
-        referrer = Customer.create(email: referrer_email) if referrer.nil?
-      end
+      # raise emails.inspect
+      customer_email = emails.first
+      # customer_email = emails.detect{ |e| emails.count(e) > 1}
+      # referrer_email = emails.detect{ |e| emails.count(e) == 1}
+      # referrer = nil
+      # if referrer_email.present?
+      #   referrer = Customer.where(email: referrer_email).first
+      #   referrer = Customer.create(email: referrer_email) if referrer.nil?
+      # end
       customer = Customer.where(email: customer_email).first
       customer = Customer.new(email: customer_email) if customer.nil?
-      customer.referred_by(referrer.id) if referrer.present?
+      # customer.referred_by(referrer.id) if referrer.present?
       customer.save
       Order.create(customer: customer)
     end
